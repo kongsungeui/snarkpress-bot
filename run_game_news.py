fcfdeb1f02f1219aaeed2653974e8d2556a2925a
Kong, SungEui <kkongchi@gmail.com>
2025-12-19 07:43:18 +0000

    run code

---
+++ run_game_news.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+🎮 글로벌 게임 뉴스 자동 포스팅 실행 엔트리
+"""
+
+from app.jobs.job_game_news import run
+
+if __name__ == "__main__":
+    run()
---
+++ run_us_tech.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+🇺🇸 미국 테크 뉴스 자동 포스팅 실행 엔트리
+"""
+
+from app.jobs.job_us_tech import run
+
+if __name__ == "__main__":
+    run()
